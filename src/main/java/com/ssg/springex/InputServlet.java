86463848da7110bf878161bea96fe4b9f64c5a5d
DaHye <heny993472@gmail.com>
2025-10-21 16:20:10 +0900

    4

---
+++ src/main/java/com/ssg/springex/InputServlet.java
@@ -0,0 +1,30 @@
+package com.ssg.springex;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String[] subject = req.getParameterValues("subject");
+        for (String s : subject) {
+            System.out.println("선택한 언어 : " + s);
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
---
+++ src/main/java/com/ssg/springex/LoginServlet_all.java
@@ -0,0 +1,41 @@
+package com.ssg.springex;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login1")
+public class LoginServlet_all extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("LoginServlet init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("LoginServlet doGet() 메소드 호출");
+        doHandle(request,response);
+    }
+
+    private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String username = request.getParameter("user_id");
+        System.out.println("doHandle 메소드 호출");
+        String password = request.getParameter("user_pwd");
+
+        System.out.println(username);
+        System.out.println(password);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost메소드 호출");
+    }
+}
